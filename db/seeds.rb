b39655004f18c6924f060b9883905d002c30e59d
Myles Amphlett <mylesamphlett@gmail.com>
2024-12-10 10:57:42 +0000

    resolves conflicts with merge with master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-puts "Starting seeds..."
-=======
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-require 'faker'
-
 Book.destroy_all
+User.destroy_all
 
 puts "Seeding books..."
->>>>>>> master
 
 15.times do
   Book.create!(
     title: Faker::Book.title,
     author: Faker::Book.author,
-<<<<<<< HEAD
+    year_published: rand(1900..2023),
     genre: Faker::Book.genre,
     summary: Faker::Lorem.sentence,
     page_count: rand(120..600),
@@ -29,21 +15,7 @@ puts "Seeding books..."
   )
 end
 
-puts "Database seeded!"
-=======
-    year_published: rand(1900..2023),
-    genre: Faker::Book.genre,
-    page_count: rand(100..1000),
-    overall_rating: rand(1..10),
-    summary: Faker::Lorem.paragraph(sentence_count: 5)
-  )
-end
-
 puts "Seeding complete! #{Book.count} books created."
-
-
-User.destroy_all
-
 puts "Seeding users..."
 
 10.times do
@@ -58,4 +30,3 @@ puts "Seeding users..."
 end
 
 puts "Seeding complete! #{User.count} users created."
->>>>>>> master
