94640ba8a7fb51fe774304e8ebb4c15bfa494af7
Myles Amphlett <mylesamphlett@gmail.com>
2024-12-10 14:16:38 +0000

    confused.com

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,22 @@
+require "json"
+require "open-uri"
+
 class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    if params[:query].present? || params[:genre_query].present?
-      @books = @books.search_by_title(params[:query]) if params[:query].present?
-      @books = @books.search_by_genre(params[:genre_query]) if params[:genre_query].present?
-    end
+    # if params[:query].present? || params[:genre_query].present?
+    #   @books = @books.search_by_title(params[:query]) if params[:query].present?
+    #   @books = @books.search_by_genre(params[:genre_query]) if params[:genre_query].present?
+    # end
+
   end
 
   def show
     @book = Book.find(params[:id])
   end
+
+  private
+
+
 end
