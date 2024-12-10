eb98f421e5f2f2cffd89ae7f5ba659f4d6db0f4d
Myles Amphlett <mylesamphlett@gmail.com>
2024-12-10 23:06:05 +0000

    yes now i've almost sorted it

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,84 +11,48 @@ class BooksController < ApplicationController
     if params[:query].present?
       @books = title_search(params[:query])
     elsif params[:genre_query].present?
-      @books = []
-      url = "https://openlibrary.org/search.json?subject=#{params[:genre_query]}&fields=key,title,author_name,isbn"
-      response = URI.parse(url).read
-      data = JSON.parse(response)
-      data["docs"].each do |d|
-        if d["isbn"]
-          book = Book.new
-          cover_url = "https://covers.openlibrary.org/b/isbn/#{d["isbn"][0]}-M.jpg"
-          book[:cover_url] = cover_url
-          @books << book
-        end
-      end
+      @books = genre_search(params[:genre_query])
     end
   end
 
   def show
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     @review = Review.new
-    key_url = "https://openlibrary.org#{params[:key]}.json"
-    response = URI.parse(key_url).read
-    data = JSON.parse(response)
+    # key_url = "https://openlibrary.org#{params[:key]}.json"
+    # response = URI.parse(key_url).read
+    # data = JSON.parse(response)
 
-    url = "https://openlibrary.org/search.json?q=#{data["title"]}&fields=key,title,author_name,isbn"
-    response = URI.parse(url).read
-    data = JSON.parse(response)
-    first = data["docs"].first
-    @book = Book.new(title: first["title"], author: first["author_name"][0])
+    # url = "https://openlibrary.org/search.json?q=#{data["title"]}&fields=key,title,author_name,isbn"
+    # response = URI.parse(url).read
+    # data = JSON.parse(response)
+    # first = data["docs"].first
+    # @book = Book.new(title: first["title"], author: first["author_name"][0])
   end
 
   private
 
+  # Here be dragons. Pls don't touch this or i will cry, ta
   def title_search(query)
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{(query)}&startIndex=0&maxResults=40&key=#{ENV["GOOGLE_API_KEY"]}"
+    books = base_search(query, url)
+  end
+
+  def genre_search(query)
+    url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{query}&startIndex=0&maxResults=40&key=#{ENV["GOOGLE_API_KEY"]}"
+    books = base_search(query, url)
+  end
+
+  def base_search(query, url)
     books = []
-    url = "https://openlibrary.org/search.json?q=#{query}&fields=key,title,author_name,isbn"
     response = URI.parse(url).read
     data = JSON.parse(response)
-    data["docs"].each do |d|
-      if d["isbn"]
-        book = Book.new
-        cover_url = "https://covers.openlibrary.org/b/isbn/#{d["isbn"][0]}-M.jpg"
-        book[:cover_url] = cover_url
-        book.key = d["key"]
-        books << book
-      end
+    data["items"].each do |d|
+      book = Book.new
+      cover_url =  d["volumeInfo"]["imageLinks"]&.dig("smallThumbnail")
+      book[:cover_url] = cover_url
+      book.key = d["id"]
+      books << book
     end
     books
   end
-
-  def genre_search
-  end
 end
-
-
-# if params[:query].present?
-#   @books = []
-#   url = "https://openlibrary.org/search.json?q=#{params[:query]}&fields=key,title,author_name,isbn"
-#   response = URI.parse(url).read
-#   data = JSON.parse(response)
-#   data["docs"].each do |d|
-#     if d["isbn"]
-#       book = Book.new
-#       cover_url = "https://covers.openlibrary.org/b/isbn/#{d["isbn"][0]}-M.jpg"
-#       book[:cover_url] = cover_url
-#       book.key = d["key"]
-#       @books << book
-#     end
-#   end
-# elsif params[:genre_query].present?
-#   @books = []
-#   url = "https://openlibrary.org/search.json?subject=#{params[:genre_query]}&fields=key,title,author_name,isbn"
-#   response = URI.parse(url).read
-#   data = JSON.parse(response)
-#   data["docs"].each do |d|
-#     if d["isbn"]
-#       book = Book.new
-#       cover_url = "https://covers.openlibrary.org/b/isbn/#{d["isbn"][0]}-M.jpg"
-#       book[:cover_url] = cover_url
-#       @books << book
-#     end
-#   end
-# end
