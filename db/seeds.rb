78f3b89c346d013b32fb54974ff0d324486c5e93
Myles Kuhns <myles@kuhns.de>
2024-12-10 11:31:11 +0100

    show page added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Book.destroy_all
+
+puts "Seeding books..."
+
+15.times do
+  Book.create!(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    year_published: rand(1900..2023),
+    genre: Faker::Book.genre,
+    page_count: rand(100..1000),
+    overall_rating: rand(1..10),
+    summary: Faker::Lorem.paragraph(sentence_count: 5)
+  )
+end
+
+puts "Seeding complete! #{Book.count} books created."
+
+
+User.destroy_all
+
+puts "Seeding users..."
+
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.unique.username(specifier: 5..8),
+    email: Faker::Internet.unique.email,
+    password: 'password123',
+    password_confirmation: 'password123'
+  )
+end
+
+puts "Seeding complete! #{User.count} users created."
