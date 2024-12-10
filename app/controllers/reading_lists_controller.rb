16b172c82d49829d6f911348b7143978f87277a5
Armon Kaizman <armon.kaizman@gmail.com>
2024-12-10 11:37:44 +0100

    unfinished work on reading_lists

--- app/controllers/reading_lists_controller.rb
+++ app/controllers/reading_lists_controller.rb
@@ -1,2 +1,30 @@
 class ReadingListsController < ApplicationController
+  before_action :deactivate
+
+  def index
+    @reading_lists = ReadingList.where(active: true)
+  end
+
+  def create
+    @reading_list = ReadingList.new(reading_list_params)
+    if @reading_list.save
+      redirect_to reading_lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def deactivate
+    @reading_list.active = false
+  end
+
+  private
+
+  def reading_list_params
+    params.require(:reading_list).permit(:user_id, :book_id, :active)
+  end
+
+  def set_reading_list
+    @reading_list = ReadingList.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# puts "Clearing database..."
+# Book.destroy_all
+# puts "Database cleared!"
+
+puts "Starting seeds..."
+15.times do
+  Book.create!(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    genre: Faker::Book.genre,
+    summary: Faker::Lorem.sentence,
+    page_count: rand(120..600),
+    overall_rating: rand(0..5)
+  )
+end
+puts "Database seeded!"
