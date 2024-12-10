1e5620a76f9c9af25d1eeed5a32c43b2cf6306b2
Myles Amphlett <mylesamphlett@gmail.com>
2024-12-10 17:08:41 +0000

    added api searches for genres and title

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,10 @@ require "open-uri"
 class BooksController < ApplicationController
 
   def index
+    # if no search queries
     @books = Book.all
-    # if params[:query].present? || params[:genre_query].present?
-    #   @books = @books.search_by_title(params[:query]) if params[:query].present?
-    #   @books = @books.search_by_genre(params[:genre_query]) if params[:genre_query].present?
-    # end
 
-    # Please don't touch any of this or I will cry 
+    # Please don't touch any of this or I will cry
     if params[:query].present?
       @books = []
       url = "https://openlibrary.org/search.json?q=#{params[:query]}&fields=key,title,author_name,isbn"
@@ -24,8 +21,19 @@ class BooksController < ApplicationController
           @books << book
         end
       end
-      # @book[:author] = book["docs"][0]["author_name"]
-      # @book[:cover_url] = "https://covers.openlibrary.org/b/isbn/#{book["docs"][0]["isbn"][0]}-M.jpg"
+    elsif params[:genre_query].present?
+      @books = []
+      url = "https://openlibrary.org/search.json?subject=#{params[:genre_query]}&fields=key,title,author_name,isbn"
+      response = URI.parse(url).read
+      data = JSON.parse(response)
+      data["docs"].each do |d|
+        if d["isbn"]
+          book = Book.new
+          cover_url = "https://covers.openlibrary.org/b/isbn/#{d["isbn"][0]}-M.jpg"
+          book[:cover_url] = cover_url
+          @books << book
+        end
+      end
     end
   end
 
@@ -33,5 +41,4 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @review = Review.new
   end
-
 end
