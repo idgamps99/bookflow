2289c1b5b9c763d5590ffecb0b658891e6677a8d
Armon Kaizman <armon.kaizman@gmail.com>
2024-12-11 15:12:02 +0100

    unfinisched sessions

--- app/models/reading_session.rb
+++ app/models/reading_session.rb
@@ -1,6 +1,6 @@
 class ReadingSession < ApplicationRecord
   belongs_to :tracker
 
-  validates :session_start, presence: true
+  #validates :session_start, presence: true
   validates :tracker, presence: true
 end
