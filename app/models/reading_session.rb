81ee8eddcd42edd2e829861c653f74d1a2e74aac
Armon Kaizman <armon.kaizman@gmail.com>
2024-12-13 12:33:39 +0100

    fine uning the stats with seeds

--- app/models/reading_session.rb
+++ app/models/reading_session.rb
@@ -4,4 +4,12 @@ class ReadingSession < ApplicationRecord
   # validates :session_start, presence: true
   validates :tracker, presence: true
   # validates :page_count, numericality: { only_integer: true, greater_than: 0 }
+  after_commit :update_tracker
+
+  private
+
+  def update_tracker
+    duration = tracker.reading_sessions.sum(:duration)
+    tracker.update(total_minutes_spent: duration)
+  end
 end
